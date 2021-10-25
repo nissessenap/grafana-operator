6473b71e723080dc04cc6cc77a161ca25c6583ff
Edvin Norling <edvin.norling@xenit.se>
2021-10-25 22:24:20 +0200

    Trying to increase the test to check service

--- controllers/grafana/grafana_controller_test.go
+++ controllers/grafana/grafana_controller_test.go
@@ -21,9 +21,11 @@ import (
 	"time"
 
 	integreatlyorgv1alpha1 "github.com/grafana-operator/grafana-operator/v4/api/integreatly/v1alpha1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -32,8 +34,9 @@ var _ = Describe("Grafana controller", func() {
 
 	// Define utility constants for object names and testing timeouts/durations and intervals.
 	const (
-		GrafanaName      = "test-grafana"
-		GrafanaNamespace = "default"
+		GrafanaName        = "test-grafana"
+		GrafanaServiceName = "grafana-service"
+		GrafanaNamespace   = "default"
 
 		timeout  = time.Second * 10
 		duration = time.Second * 10
@@ -67,11 +70,19 @@ var _ = Describe("Grafana controller", func() {
 							},
 						},
 					},
+					Service: &integreatlyorgv1alpha1.GrafanaService{
+						Name: "grafana-service",
+						Labels: map[string]string{
+							"app":  "grafana",
+							"type": "grafana-service",
+						},
+					},
 				},
 			}
 			Expect(k8sClient.Create(ctx, grafana)).Should(Succeed())
 
 			grafanaLookupKey := types.NamespacedName{Name: GrafanaName, Namespace: GrafanaNamespace}
+			//grafanaServiceLookupKey := types.NamespacedName{Name: GrafanaName, Namespace: GrafanaNamespace}
 			createdGrafana := &integreatlyorgv1alpha1.Grafana{}
 
 			// We'll need to retry getting this newly created CronJob, given that creation may not immediately happen.
@@ -84,65 +95,41 @@ var _ = Describe("Grafana controller", func() {
 			}, timeout, interval).Should(BeTrue())
 			// Let's make sure our Schedule string value was properly converted/handled.
 			Expect(createdGrafana.ObjectMeta.Name).Should(Equal("test-grafana"))
+			//By("By checking the service that got created")
 
 			/*
-				By("By checking the CronJob has zero active Jobs")
-				Consistently(func() (int, error) {
-					err := k8sClient.Get(ctx, grafanaLookupKey, createdGrafana)
-					if err != nil {
-						return -1, err
-					}
-					return len(createdGrafana.Status.Active), nil
-				}, duration, interval).Should(Equal(0))
-
-				By("By creating a new Job")
-				testJob := &batchv1.Job{
+				createdService := &apiv1.Service{
 					ObjectMeta: metav1.ObjectMeta{
-						Name:      GrafanaName,
+						Name:      GrafanaServiceName,
 						Namespace: GrafanaNamespace,
 					},
-					Spec: batchv1.JobSpec{
-						Template: v1.PodTemplateSpec{
-							Spec: v1.PodSpec{
-								// For simplicity, we only fill out the required fields.
-								Containers: []v1.Container{
-									{
-										Name:  "test-container",
-										Image: "test-image",
-									},
-								},
-								RestartPolicy: v1.RestartPolicyOnFailure,
+					Spec: apiv1.ServiceSpec{
+						Ports: []apiv1.ServicePort{
+							{
+								Protocol:   "TCP",
+								Port:       3000,
+								TargetPort: intstr.FromString("grafana-http"),
 							},
 						},
 					},
-					Status: batchv1.JobStatus{
-						Active: 2,
-					},
 				}
-
-				// Note that your CronJob’s GroupVersionKind is required to set up this owner reference.
-				kind := reflect.TypeOf(cronjobv1.CronJob{}).Name()
-				gvk := cronjobv1.GroupVersion.WithKind(kind)
-
-				controllerRef := metav1.NewControllerRef(createdGrafana, gvk)
-				testJob.SetOwnerReferences([]metav1.OwnerReference{*controllerRef})
-				Expect(k8sClient.Create(ctx, testJob)).Should(Succeed())
-
-				By("By checking that the CronJob has one active Job")
-				Eventually(func() ([]string, error) {
-					err := k8sClient.Get(ctx, cronjobLookupKey, createdGrafana)
-					if err != nil {
-						return nil, err
-					}
-
-					names := []string{}
-					for _, job := range createdGrafana.Status.Active {
-						names = append(names, job.Name)
-					}
-					return names, nil
-				}, timeout, interval).Should(ConsistOf(JobName), "should list our active job %s in the active jobs list in status", JobName)
 			*/
+
+			//createdService := &apiv1.Service{}
+			//podList := &corev1.PodList{}
+			createdService := &corev1.ServiceList{}
+
+			Eventually(func() bool {
+				//err := k8sClient.Get(ctx, grafanaServiceLookupKey, createdService)
+				err := k8sClient.List(ctx, createdService, &client.ListOptions{})
+				if err != nil {
+					return false
+				}
+				return true
+			}, timeout, interval).Should(BeTrue())
+			// Let's make sure our Schedule string value was properly converted/handled.
+			Expect(createdService.Items).Should(Equal(GrafanaServiceName))
+
 		})
 	})
-
 })
